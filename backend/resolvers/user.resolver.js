87d7ae26e085281b315e2cf42b864b91c6dd0ca1
Jackson Tran <javtran@ucsc.edu>
2024-07-20 19:13:57 -0700

    register mutation

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,4 +1,6 @@
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const userResolver = {
   Query: {
@@ -9,7 +11,47 @@ const userResolver = {
       return users.find((user) => user._id === userId);
     },
   },
-  Mutation: {},
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+
+        // Validate inputs
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fields are required");
+        }
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          throw new Error("Username already exists");
+        }
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // https://avatar-placeholder.iran.liara.run/
+        // Get profile pic
+        const profilePic = `https://avatar.iran.liara.run/public/${
+          gender == "male" ? "boy" : "girl"
+        }?username=${username}`;
+
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: profilePic,
+        });
+
+        await newUser.save();
+        await context.login(user);
+        return newUser;
+      } catch (error) {
+        console.error("Error in signup", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    },
+  },
 };
 
 export default userResolver;
